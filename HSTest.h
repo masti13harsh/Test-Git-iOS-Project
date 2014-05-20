002363bf9338a96f83689179432ae818d93772af
Harsh Sapra <harshsapra@Harshs-MacBook-Pro.local>
2014-05-20 15:51:34 +0530

    added a test class

---
+++ HSTest.h
@@ -0,0 +1,13 @@
+//
+//  HSTest.h
+//  Git Test
+//
+//  Created by Harsh Sapra on 20/05/14.
+//  Copyright (c) 2014 Harsh Sapra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HSTest : NSObject
+
+@end
