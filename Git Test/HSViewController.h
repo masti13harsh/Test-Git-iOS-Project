bb2abadca142beeb3ce7268329590e66d7d22ba5
Harsh Sapra <harshsapra@Harshs-MacBook-Pro.local>
2014-05-20 15:36:09 +0530

    Initial Commit

---
+++ Git Test/HSViewController.h
@@ -0,0 +1,13 @@
+//
+//  HSViewController.h
+//  Git Test
+//
+//  Created by Harsh Sapra on 20/05/14.
+//  Copyright (c) 2014 Harsh Sapra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Harsh Sapra on 20/05/14.
+//  Copyright (c) 2014 Harsh Sapra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HSAppDelegate class]));
+    }
+}
